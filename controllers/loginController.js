75391638f5576a7a1f55168127707cea8c2f40ba
Valentin <valentin.renaudin@my-digital-school.org>
2024-02-23 11:52:29 +0100

    [FIX] Le user pouvait se connecter avec n'importe quel mdp

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -8,32 +8,39 @@ dotenv.config({path : '.env.local'});
 // URL de connexion à la base de données MongoDB
 const url = 'mongodb://127.0.0.1:27017';
 
+const jwtSecretKey = process.env.TOKEN_KEY;
+
 const dbName = 'api-mongo';
 
 //Fonction pour authentifier un utilisateur
 
 exports.authentification = async (req, res) => {
-
-    const login= req.body.email;
+    const email = req.body.email;
     const password = req.body.password;
+  
     const client = new MongoClient(url);
-    await client.connect();
-
+    
     try {
-        const db = client.db(dbName);
-        const collection = db.collection('users');
-        const user = await collection.find({ login, password });
-        if (!user) {
-            return res.status(401).json({ message: 'Email ou mot de passe incorrect.' });
-        }
-        const token = jwt.sign({ email: user.email }, process.env.TOKEN_KEY, { expiresIn: '24h' });
-        res.status(200).json({ message: 'Authentification réussie.', token: token });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Erreur lors de l\'authentification.' });
+      await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection("users");
+      const user = await collection.findOne({ email: email });
+  
+      if (user && user.password === password) {
+        // Passwords match, authentication successful
+        const token = jwt.sign({ email: user.email }, jwtSecretKey, { expiresIn: '1h' });
+        res.status(200).json({ token });
+      } else {
+        // Either user not found or password incorrect
+        res.status(401).json("Identifiants incorrects");
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).send(e);
     } finally {
-        await client.close();
+      // Ensure to close the connection
+      await client.close();
     }
-}
+  };
 
 module.exports = exports;
